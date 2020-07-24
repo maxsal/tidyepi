3b5bff58ae8683a70149e318b1fd87fe801f281b
Max Salvatore <maxsalvatore@maxs-mbp.attlocal.net>
2020-07-24 17:25:29 -0400

    add support for dummy variables

--- ct_2x2.R
+++ ct_2x2.R
@@ -1,15 +1,39 @@
 library(tidyverse)
 library(glue)
 
-ct_2x2 <- function(x,
+ct_2x2 <- function(x, dataset = NULL,
                    conf = 0.95) {
   
   out <- list()
+  
+  if (is.numeric(x) & length(x) == 4) { 
   
   a <- x[1]
   b <- x[2]
   c <- x[3]
   d <- x[4]
+  
+  y <- x
+  
+  } else if (is.character(x) & length(x) == 2) {
+    
+    if ( all(unique(dataset[[x[1]]]) == c(0, 1)) &
+         all(unique(dataset[[x[2]]]) == c(0, 1))
+         ) {
+      
+      tab <- table(dataset[[x[1]]], dataset[[x[2]]])
+        
+      a <- tab["1", "1"]
+      b <- tab["1", "0"]
+      c <- tab["0", "1"]
+      d <- tab["0", "0"]
+      
+      y <- c(a, b, c, d)
+      
+    }
+    
+  }
+
   e <- a + b
   f <- c + d
   g <- a + c
@@ -22,11 +46,11 @@ ct_2x2 <- function(x,
   out[["cross_tab"]] <- tibble::tibble(
     exposure,
     outcome,
-    x
+    y
   ) %>%
     stats::xtabs(
       data = .,
-      x ~ exposure + outcome
+      y ~ exposure + outcome
     )
   
   or <- (a * d) / (b * c)
